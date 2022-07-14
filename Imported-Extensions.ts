90394536224426038685cd799a6f916b1d331355
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-07-13 21:52:54 -0400

    Initial files for MakeCode project

---
+++ Imported-Extensions.ts
@@ -0,0 +1,18 @@
+// Copy of extension(s) that are used in my project, but won't be seen in the toolbox
+namespace sprites {
+    export function setDataNumber(sprite: Sprite, name: string, value: number) {
+        if (!sprite || !name) return;
+        const d = sprite.data;
+        d[name] = value;
+    }
+    export function changeDataNumberBy(sprite: Sprite, name: string, value: number) {
+        if (!sprite || !name) return;
+        const d = sprite.data;
+        d[name] = (d[name] || 0) + value;
+    }
+    export function readDataNumber(sprite: Sprite, name: string): number {
+        if (!sprite || !name) return 0;
+        const d = sprite.data;
+        return d[name] as number;
+    }
+}
