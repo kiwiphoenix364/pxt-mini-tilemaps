6f81a260de864599a516eecb646582597391f6e0
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-07-15 23:56:19 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -52,7 +52,7 @@ game.onUpdate(function () {
             canx = 1
             cany = 1
             for (let index = 0; index <= repeat; index++) {
-                if (index > 0) {
+                if (index > 0 && repeat > 0) {
                     if (canx == 1) {
                         mySprite.x += (curx - sprites1.readDataNumber(mySprite, "prevx")) / repeat
                         for (let index2 = 0; index2 <= TileCollisionArrayX.length; index2++) {
