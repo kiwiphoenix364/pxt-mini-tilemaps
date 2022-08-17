7ebc21fd1576c247adc7bad388d165a2248d6113
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-08-16 22:42:19 -0400

    Update main.ts, OnGameUpdate.ts

--- main.ts
+++ main.ts
@@ -26,25 +26,4 @@ namespace MiniTilemaps {
     }
     
     // Small useful functions from extensions used in my extension, credit to MakeCode devs.
-    export function setDataNumber(sprite: Sprite, name: string, value: number) {
-        if (!sprite || !name) return;
-        const d = sprite.data;
-        d[name] = value;
-    }
-    export function changeDataNumberBy(sprite: Sprite, name: string, value: number) {
-        if (!sprite || !name) return;
-        const d = sprite.data;
-        d[name] = (d[name] || 0) + value;
-    }
-    export function readDataNumber(sprite: Sprite, name: string): number {
-        if (!sprite || !name) return 0;
-        const d = sprite.data;
-        return d[name] as number;
-    }
-    export function onMapLoaded(cb: (tilemap: tiles.TileMapData) => void) {
-        tiles.addEventListener(tiles.TileMapEvent.Loaded, cb);
-    }
-    export function onMapUnloaded(cb: (tilemap: tiles.TileMapData) => void) {
-        tiles.addEventListener(tiles.TileMapEvent.Unloaded, cb);
-    }
 }
